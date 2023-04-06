d90fac5008972083dfc3f9857ebd420492837255
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-04-06 14:17:19 -0400

    login page refactored

--- src/main/byitacademy/habasaraba/finalproject/leviPages/LogInPage.java
+++ src/main/byitacademy/habasaraba/finalproject/leviPages/LogInPage.java
@@ -8,7 +8,7 @@ public class LogInPage {
     public static final String INPUT_EMAIL="//div[@id='login-email']";
     public static final String INPUT_PASSWORD="//input[@type='password']";
     public static final String LOGIN_BTN="//button[text()='Log In']";
-    public static final String HELLO_USER = "//*[@id=\"greeting-link-dropdown\"]";
+    public static final String HELLO_USER = "//*[@id='greeting-link-dropdown']";
     public static final String ERR_MSG_PASSWORD="//div[@class='err-msg']";
     public static final String ERR_MSG_EMAIL="//div[@class='err-msg']";
 
