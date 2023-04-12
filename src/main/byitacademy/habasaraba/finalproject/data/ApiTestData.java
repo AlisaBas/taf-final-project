cac69d0fa769e7bb5dbdacbcbeb2e3b1fb5eda43
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-04-12 17:46:56 -0400

    API tests post

--- src/main/byitacademy/habasaraba/finalproject/data/ApiTestData.java
+++ src/main/byitacademy/habasaraba/finalproject/data/ApiTestData.java
@@ -1,5 +1,6 @@
 package byitacademy.habasaraba.finalproject.data;
 
 public class ApiTestData {
-    public static final String API_URL="levi.com/nextgen-webhooks";
+    public static final String API_URL_SUBSCRIPTION="https://www.levi.com/nextgen-webhooks/?operationName=checkSubscriptionStatus&locale=US-en_US";
+    public static final String API_URL_LOGIN="https://www.levi.com/nextgen-webhooks/?locale=US-en_US&operationName=login";
 }
--- src/test/byitacademy/habasaraba/finalproject/ApiTest.java
+++ src/test/byitacademy/habasaraba/finalproject/ApiTest.java
@@ -1,27 +1,46 @@
 package byitacademy.habasaraba.finalproject;
 import byitacademy.habasaraba.finalproject.data.ApiTestData;
-import byitacademy.habasaraba.finalproject.data.Item;
+import byitacademy.habasaraba.finalproject.data.UserData;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.equalTo;
 
 
 public class ApiTest {
- @Test
-    public void testGetUrl(){
-     String url="https://www.levi.com/";
-     when().get(url).
-             then().statusCode(200);
- }
     @Test
-    public void testGetJeans() {
-        String url = "https://www.levi.com/US/en_US/search/men%20jeans";
-        int id = 1;
+    public void testGetUrl() {
+        String url = "https://www.levi.com/";
         when().get(url).
-                then().assertThat().
-                statusCode(200);
+                then().statusCode(200);
+    }
 
-    }}
+    @Test
+    public void testPostInvalidUserSubscriptionWithoutBody() {
+        given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                when().
+                post(ApiTestData.API_URL_SUBSCRIPTION).
+                then().log().body().
+                assertThat().statusCode(500);
+    }
+
+    @Test
+    public void testPostInvalidUserLogin() {
+        RestAssured
+                .given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                multiPart("email", UserData.EMAIL_INVALID).
+                multiPart("password", UserData.PASSWORD_INVALID);
+        when().
+                post(ApiTestData.API_URL_LOGIN).
+                then().log().body().
+                assertThat().statusCode(403);
+    }
+}
 
 
 
